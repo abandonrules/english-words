2ce64b5c66ef6e92a3aad04c20f475ce3621db5d
Pandaradox <jacardarella@gmail.com>
2019-03-12 18:24:02 -0500

    Added Letter Pairing Scanner for Project

--- read_english_dictionary.py
+++ read_english_dictionary.py
@@ -1,11 +1,27 @@
 def load_words():
     with open('words_alpha.txt') as word_file:
-        valid_words = set(word_file.read().split())
+    # valid_words = set(word_file.read().split())
+        valid_words = word_file.read().split('\n')
 
     return valid_words
 
 
 if __name__ == '__main__':
-    english_words = load_words()
-    # demo print
-    print('fate' in english_words)
+    ALLWORDS = load_words()
+    pairs = {}
+    # print(len(ALLWORDS))
+    # 370100 words in list
+
+    for x in ALLWORDS:
+        x = x.lower()
+        print(x)
+        for i in range(len(x)-1):
+            if (x[i:i+2] in pairs):
+                pairs[x[i:i+2]] = pairs.get(x[i:i+2]) + 1
+            else:
+                pairs[x[i:i+2]] = 1
+
+    minimumCount = 10000
+    for pair in sorted(pairs):
+        if pairs.get(pair) > minimumCount:
+            print(pair, pairs.get(pair))
